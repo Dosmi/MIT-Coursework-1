7e6db2d9d9e018745405004c473d731285c447f0
sc17dd <dom@localhost.localdomain>
2019-11-01 22:41:47 +0000

    working on generalized cylinders

--- surf.cpp
+++ surf.cpp
@@ -12,11 +12,13 @@ namespace
         // cerr << "profile size: " << profile.size() << endl;
         for (unsigned i=0; i<profile.size(); i++)
         { // if profile is empty, just returns true (why it works without any normals etc ...
-          // cerr << "V: "; profile[i].V.print();
-          // cerr << "T: "; profile[i].T.print();
-          // cerr << "N: "; profile[i].N.print();
+          cerr << "V: "; profile[i].V.print();
+          cerr << "T: "; profile[i].T.print();
+          cerr << "N: "; profile[i].N.print();
+          cerr << "B: "; profile[i].B.print();
+          cerr << endl;
 
-          cerr << i << ") V:" << profile[i].V[2] << " T:" << profile[i].T[2] << " N:" << profile[i].N[2] << endl;
+          // cerr << i << ") V:" << profile[i].V[2] << " T:" << profile[i].T[2] << " N:" << profile[i].N[2] << endl;
           if (profile[i].V[2] != 0.0 ||
               profile[i].T[2] != 0.0 ||
               profile[i].N[2] != 0.0)
@@ -95,31 +97,12 @@ Surface makeSurfRev(const Curve &profile, unsigned steps)
       unsigned rotation_number = 0;
       for ( float singleRotation = (2*3.14)/steps; singleRotation <= 2*3.14; singleRotation += (2*3.14)/steps )
       {
-          // Tup3u triangle
-          // (
-          //   (currentProfile * steps) + rotation_number,
-          //   (currentProfile * steps) + rotation_number + 1,
-          //   ((currentProfile + 1) * steps) + rotation_number + 1
-          // );
-
-          // Tup3u triangle
-          // (
-          //   ((currentProfile + 1) * steps) + rotation_number + 1,
-          //   (currentProfile * steps) + rotation_number,
-          //   (currentProfile * steps) + rotation_number + 1
-          // );
-
           Tup3u triangle
           (
             ((currentProfile + 1) * steps) + rotation_number + 1,
             (currentProfile * steps) + rotation_number + 1,
             (currentProfile * steps) + rotation_number
           );
-
-          // cerr << "----------- TRIANGLE -------------" << endl;
-          // cerr << triangle[0] << endl;
-          // cerr << triangle[1] << endl;
-          // cerr << triangle[2] << endl << endl;
 
           surface.VF.push_back(triangle);
 
@@ -153,16 +136,98 @@ Surface makeSurfRev(const Curve &profile, unsigned steps)
 *********************************************************************************************************/
 
 Surface makeGenCyl(const Curve &profile, const Curve &sweep )
-{
+{   // generalized cylinders
     Surface surface;
 
-    if (!checkFlat(profile))
+    if (!checkFlat(sweep))
     {
         cerr << "genCyl profile curve must be flat on xy plane." << endl;
         exit(0);
     }
 
     // TODO: Here you should build the surface.  See surf.h for details.
+    // std::vector< std::vector<Vector3f> > surfacePoints;
+    std::vector <Vector3f> surfacePoints;
+    for( unsigned i = 0; i < sweep.size(); i++ )
+    {
+        Matrix4f translationMatrix = Matrix4f
+            (
+              1,  0,  0, sweep[i].V[0],
+              0,  1,  0, sweep[i].V[1],
+              0,  0,  1, sweep[i].V[2],
+              0,  0,  0,  1
+            );
+        for( unsigned j = 0; j < profile.size(); j++ )
+        {
+          Matrix4f originalPoint = Matrix4f
+          (
+            1,  0,  0, profile[j].V[0],
+            0,  1,  0, profile[j].V[1],
+            0,  0,  1, profile[j].V[2],
+            0,  0,  0,  1
+          );
+          
+          Matrix4f matrixOfOnes(1);
+          Matrix4f rotateX90 = matrixOfOnes.rotateX(1.57);
+
+          Matrix4f rotatedProfile = rotateX90 * originalPoint;
+
+          // Matrix4f movedPoint = translationMatrix * originalPoint;
+Matrix4f movedPoint = translationMatrix * rotatedProfile;
+Vector4f surfacePointHG = movedPoint.getCol(3);
+
+          // and now since the point has been moved, we can simply apply a 90 degree rotation
+          // Matrix4f matrixOfOnes(1);
+          // Matrix4f rotateX90 = matrixOfOnes * matrixOfOnes.rotateX(1.5708);
+          //
+          // Matrix4f finalPointLocation = movedPoint * rotateX90;
+
+          // Vector4f surfacePointHG = finalPointLocation.getCol(3);
+          Vector3f surfacePoint(surfacePointHG[0], surfacePointHG[1], surfacePointHG[2]);
+
+          // surfacePoints.push_back(surfacePoint);
+          surface.VV.push_back(surfacePoint);
+          surface.VN.push_back(surfacePoint);
+
+          // Tup3u bottomtriangle
+          // (
+          //   ((i + 1) * i) + i,
+          //   ((i + 1) * i) + i + 1,
+          //   (i * i) + i
+          // );
+          //
+          // surface.VF.push_back(bottomtriangle);
+        }
+    }
+
+    // have a separate part FOR NOW, to be move, this is experiemental ...
+    int steps = profile.size();
+    for( unsigned currentProfile = 0; currentProfile < profile.size(); currentProfile++ )
+    {
+      unsigned rotation_number = 0;
+      for( unsigned i = 0; i < sweep.size(); i++ )
+      {
+          Tup3u triangle
+          (
+            ((currentProfile + 1) * steps) + rotation_number + 1,
+            (currentProfile * steps) + rotation_number + 1,
+            (currentProfile * steps) + rotation_number
+          );
+
+          surface.VF.push_back(triangle);
+
+          Tup3u bottomtriangle
+          (
+            ((currentProfile + 1) * steps) + rotation_number,
+            ((currentProfile + 1) * steps) + rotation_number + 1,
+            (currentProfile * steps) + rotation_number
+          );
+
+          surface.VF.push_back(bottomtriangle);
+
+          rotation_number++;
+      }
+    }
 
     cerr << "\t>>> makeGenCyl called (but not implemented).\n\t>>> Returning empty surface." <<endl;
 
